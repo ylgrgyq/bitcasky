b5c5f50a75837d81fb09a1dc7456dbf2725d6378
ylgrgyq <gr@ylgrgyq.com>
2023-04-19 23:53:11 +0800

    generate random

--- bitcask-tests/src/common.rs
+++ bitcask-tests/src/common.rs
@@ -1,6 +1,8 @@
-use rand::Rng;
+use bytes::BytesMut;
+use rand::RngCore;
 use std::path::PathBuf;
 
+#[derive(Clone)]
 pub struct TestingKV {
     key: Vec<u8>,
     value: Vec<u8>,
@@ -13,6 +15,14 @@ impl TestingKV {
             value: v.into(),
         }
     }
+
+    pub fn from_bytes(k: &[u8], v: &[u8]) -> TestingKV {
+        TestingKV {
+            key: k.to_vec(),
+            value: v.to_vec(),
+        }
+    }
+
     pub fn key(&self) -> Vec<u8> {
         self.key.clone()
     }
@@ -26,6 +36,23 @@ pub fn get_temporary_directory_path() -> PathBuf {
     dir.into_path()
 }
 
-pub fn generate_random_testing_kvs(key_size: usize, value_size: usize, kvs_len: usize) {
+pub fn generate_random_testing_kvs(
+    key_size: usize,
+    value_size: usize,
+    kvs_len: usize,
+) -> Vec<TestingKV> {
+    let mut ret: Vec<TestingKV> = vec![generate_random_testing_kv(key_size, value_size); kvs_len];
+    ret.fill_with(|| generate_random_testing_kv(key_size, value_size));
+    ret
+}
+
+pub fn generate_random_testing_kv(key_size: usize, value_size: usize) -> TestingKV {
     let mut rng = rand::thread_rng();
+    let mut k = BytesMut::with_capacity(key_size);
+    rng.fill_bytes(k.as_mut());
+
+    let mut v = BytesMut::with_capacity(value_size);
+    rng.fill_bytes(v.as_mut());
+
+    TestingKV::from_bytes(&k, &v)
 }
