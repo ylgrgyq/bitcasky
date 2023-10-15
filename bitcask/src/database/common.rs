53b90b6c6b03bc2ff84cb9f7bc815c8afc7ef619
ylgrgyq <alven.gr@gmail.com>
2023-10-15 23:07:56 +0800

    remove unused

--- bitcask/src/database/common.rs
+++ bitcask/src/database/common.rs
@@ -1,25 +1,14 @@
 use std::{
-    fs::File,
-    io::{ErrorKind, Read, Seek, SeekFrom},
     ops::Deref,
-    path::Path,
     time::{Duration, SystemTime, UNIX_EPOCH},
-    vec,
 };
 
-use bytes::{Buf, Bytes, BytesMut};
+use bytes::{Bytes, BytesMut};
 use crc::{Crc, CRC_32_CKSUM};
 
-use crate::{
-    error::{BitcaskError, BitcaskResult},
-    file_id::FileId,
-    utils::TOMBSTONE_VALUE,
-};
+use crate::{error::BitcaskResult, file_id::FileId, utils::TOMBSTONE_VALUE};
 
-use super::constants::{
-    DATA_FILE_KEY_OFFSET, DATA_FILE_KEY_SIZE_OFFSET, DATA_FILE_TSTAMP_OFFSET,
-    DATA_FILE_VALUE_SIZE_OFFSET, KEY_SIZE_SIZE, VALUE_SIZE_SIZE,
-};
+use super::constants::DATA_FILE_KEY_OFFSET;
 
 pub trait Encoder<T> {
     fn encode(obj: T) -> Bytes;
@@ -80,68 +69,6 @@ impl<'a, V: Deref<Target = [u8]>> RowToWrite<'a, V> {
         bs.extend_from_slice(&self.value);
         bs.freeze()
     }
-}
-
-pub fn io_error_to_bitcask_error(
-    database_dir: &Path,
-    file_id: FileId,
-    e: std::io::Error,
-    hint: &str,
-) -> BitcaskError {
-    match e.kind() {
-        ErrorKind::UnexpectedEof => {
-            return BitcaskError::DataFileCorrupted(
-                database_dir.display().to_string(),
-                file_id,
-                hint.into(),
-            );
-        }
-        _ => BitcaskError::IoError(e),
-    }
-}
-
-pub fn read_value_from_file(
-    file_id: FileId,
-    data_file: &mut File,
-    value_offset: u64,
-    size: u64,
-) -> BitcaskResult<TimedValue<Value>> {
-    data_file.seek(SeekFrom::Start(value_offset))?;
-    let mut buf = vec![0; size as usize];
-    data_file.read_exact(&mut buf)?;
-
-    let bs = Bytes::from(buf);
-    let expected_crc = bs.slice(0..4).get_u32();
-
-    let crc32 = Crc::<u32>::new(&CRC_32_CKSUM);
-    let mut ck = crc32.digest();
-    ck.update(&bs.slice(4..));
-    let actual_crc = ck.finalize();
-    if expected_crc != actual_crc {
-        return Err(BitcaskError::CrcCheckFailed(
-            file_id,
-            value_offset,
-            expected_crc,
-            actual_crc,
-        ));
-    }
-    let timestamp = bs
-        .slice(DATA_FILE_TSTAMP_OFFSET..DATA_FILE_KEY_SIZE_OFFSET)
-        .get_u64();
-
-    let key_size = bs
-        .slice(DATA_FILE_KEY_SIZE_OFFSET..(DATA_FILE_KEY_SIZE_OFFSET + KEY_SIZE_SIZE))
-        .get_u64() as usize;
-    let val_size = bs
-        .slice(DATA_FILE_VALUE_SIZE_OFFSET..(DATA_FILE_VALUE_SIZE_OFFSET + VALUE_SIZE_SIZE))
-        .get_u64() as usize;
-    let val_offset = DATA_FILE_KEY_OFFSET + key_size;
-    let ret = bs.slice(val_offset..val_offset + val_size);
-
-    Ok(TimedValue {
-        value: Value::VectorBytes(ret.into()),
-        timestamp,
-    })
 }
 
 pub trait BitcaskDataFile {
